787493d1029a6fe611c643b4768d02b29a70921f
Nicholas Albright <nma-io@users.noreply.github.com>
2015-10-19 22:17:22 -0600

    Update ssltest.py

--- ssltest.py
+++ ssltest.py
@@ -1,51 +1,54 @@
 #!/usr/bin/env python
-#Author Prajwal Panchmahalkar
-#inspired by "TrullJ"
-#usage python ssltest.py www.google.com
-
-import requests
-import time
+# inspired by 'TrullJ'
+__author__ = 'Prajwal Panchmahalkar'
 import sys
 import json
-import warnings
-start_time = time.clock()
-warnings.filterwarnings("ignore")
-def sendReq(payload={}):
-	url = "https://api.ssllabs.com/api/v2/analyze"
-	try:
-		response = requests.get(url, params=payload)
-	except requests.exception.RequestException as e:
-		print e
-		sys.exit(1)
-	data = response.json()
-	return data
-def scanHost(host):
-	payload = {'host': host, 'publish': "off", 'startNew': "on", 'all': "done", 'ignoreMismatch': "on"}
-	results = sendReq(payload)
-	payload.pop('startNew')	
-	while results['status'] != 'READY' and results['status'] != 'ERROR':
-		time.sleep(30)
-		results = sendReq(payload)	
-	checkdata = results
-	for each in checkdata["endpoints"]:
-		if(each["statusMessage"]=="Ready"):
-			print ", IP Address ="+str(each["ipAddress"]),
-			print ", Grade = "+each["grade"],
-			try:
-				print ", POODLE = "+str(each["details"]["poodle"]),
-				print ", POODLE_TLS ="+str(each["details"]["poodleTls"]),
-				print ", RC4 Support ="+str(each["details"]["supportsRc4"]),
-				print ", BEAST ="+str(each["details"]["vulnBeast"]),
-				print ", LOGJAM ="+str(each["details"]["logjam"]),
-				print ", heartbleed ="+str(each["details"]["heartbleed"])
-			except:
-				pass
-def main():
-	if(len(sys.argv)!=2):
-		print "[!] Usage eg., python sslgrade.py www.google.com"
-		sys.exit(0)
-	else:
-		scanHost(sys.argv[1])
+import time
+import requests
 
-if __name__ == "__main__":
-	main()
+
+def sendReq(payload={}):
+    url = 'https://api.ssllabs.com/api/v2/analyze'
+    try:
+        response = requests.get(url, params=payload).json()
+        return(response)
+    except requests.exception.RequestException as e:
+        print e
+        exit(1)
+
+
+def scanHost(host):
+    r = {}
+    payload = {'host': host, 'publish': 'off', 'startNew': 'off', 'all': 'done', 'ignoreMismatch': 'on'}
+    results = sendReq(payload)
+    payload.pop('startNew')
+    interesting_tests = ['poodle', 'poodleTls', 'supportsRc4', 'vulnBeast', 'logjam', 'heartbleed']
+    while results['status'] != 'READY' and results['status'] != 'ERROR':
+        time.sleep(30)
+        results = sendReq(payload)
+    checkdata = results
+    for each in checkdata['endpoints']:
+        if 'Ready' not in each['statusMessage']: continue
+        row = each['ipAddress']
+        r.update({row: [{'IP Address': row}]})
+        r[row].append({'Grade': each['grade']})
+        for testtype in interesting_tests:
+            r[row].append({testtype.upper(): each['details'][testtype]})
+    return(r)
+
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        print 'SSLtest, %s\n\t[!] Usage eg.: python %s www.google.com' % (__author__, __file__)
+        exit()
+    output = 'json'  # Optional JSON output - leave blank for key/value pair.
+    results = scanHost(sys.argv[1])
+    if output == 'json':
+        print json.dumps(results, indent=2)
+        exit()
+    for line in results:
+        r = []
+        for x in results[line]:
+            data, value = x.items()[0]
+            r.append('{} = {}'.format(data, value))
+        print ', '.join(r)
